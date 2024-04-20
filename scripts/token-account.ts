77f60e065828eb5131bd9230598e8f385d340c4a
Denis Carriere <carriere.denis@gmail.com>
2024-04-20 11:48:53 +0200

    remove no-contract tokens

--- scripts/token-account.ts
+++ scripts/token-account.ts
@@ -2,14 +2,16 @@ import { APIClient } from "@wharfkit/antelope"
 import tokens from "../tokens.json"
 import { sleep } from "bun";
 
-const client = new APIClient({url: "https://eos.api.eosnation.io"});
 let start = false;
 for ( const token of tokens) {
-    if ( token.chain !== "eos" ) continue;
-    if ( token.symbol == "EDNA" ) start = true;
-    if ( !start ) continue;
+    const client = new APIClient({url: `https://${token.chain}.api.eosnation.io`});
+    if ( token.chain === "proton" ) continue;
+    if ( token.chain === "eos" ) continue;
+    // if ( token.symbol == "EDNA" ) start = true;
+    // if ( !start ) continue;
     try {
-        const stats = await client.v1.chain.get_currency_stats(token.account, token.symbol)
+        await client.v1.chain.get_currency_stats(token.account, token.symbol)
+        console.log(token.symbol, "✅")
     } catch (e) {
         console.log(token);
     }
