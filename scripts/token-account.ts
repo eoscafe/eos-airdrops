85136a59c85fb8a8c8807d0ea7e7842594339d2a
Denis Carriere <carriere.denis@gmail.com>
2024-05-07 14:40:23 +0200

    remove duplicates

--- scripts/token-account.ts
+++ scripts/token-account.ts
@@ -3,6 +3,7 @@ import tokens from "../tokens.json"
 import { sleep } from "bun";
 
 let start = false;
+console.log("Checking tokens if exists...")
 for ( const token of tokens) {
     const client = new APIClient({url: `https://${token.chain}.greymass.com`});
     // if ( token.chain !== "proton" ) continue;
@@ -11,8 +12,9 @@ for ( const token of tokens) {
     // if ( !start ) continue;
     try {
         await client.v1.chain.get_currency_stats(token.account, token.symbol)
-        console.log(token.symbol, "✅")
+        process.stdout.write(token.symbol + " ");
     } catch (e) {
+        console.log(token.symbol, "❌")
         console.log(token);
     }
     await sleep(100)
